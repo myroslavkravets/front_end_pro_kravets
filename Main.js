71a29501a0bcd994f499bd09766e9c58e1e72cd5
myroslavkravets <miroslavchikthebest@gmail.com>
2024-09-13 07:19:51 +0300

    lesson-5 commit

--- Main.js
+++ Main.js
@@ -1,90 +1,41 @@
-let userName = prompt("Введіть своє ім'я:");
-console.log(`Hello, ${userName}! How are you?`);
-// Запрашиваем у пользователя тризначное число
-let number = prompt("Введіть тризначне число:");
+// Завдання 1
+for (let i = 20; i <= 30; i += 0.5) {
+    console.log(i);
+}
 
-// Преобразуем введенное значение в строку, чтобы легко работать с отдельными цифрами
-let firstDigit = number[0];
-let secondDigit = number[1];
-let thirdDigit = number[2];
-
-// Проверяем, чи всі цифри однакові
-if (firstDigit === secondDigit && secondDigit === thirdDigit) {
-    console.log("Всі цифри однакові.");
+// // Завдання 2
+const rate = 26; // Курс долара (1 долар = 26 гривень)
+for (let dollars = 10; dollars <= 100; dollars += 10) {
+    let hryvnias = dollars * rate;
+    console.log(`${dollars} доларів коштує ${hryvnias} гривень`);
+}
+// // Завдання 3
+const N = +prompt("Введіть ціле число:");
+let output = ``;
+if (N >= 1) {
+    for (let i = 1; i <= 100; i++) {
+        if (i * i <= N) {
+            output += i + ' ';
+        }
+    }
+}
+console.log(output.trim());
+// // Завдання 4
+const number = +prompt("Введіть ціле число:");
+let isPrime = true;
+if (number <= 1) {
+    isPrime = false;
 } else {
-    console.log("Не всі цифри однакові.");
+    // Перевіряємо всі числа від 2 до number - 1
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
 }
-
-// Проверяем, чи є серед цифр однакові
-if (firstDigit === secondDigit || firstDigit === thirdDigit || secondDigit === thirdDigit) {
-    console.log("Є однакові цифри серед чисел.");
+if (isPrime) {
+    console.log(`${number} є простим числом.`);
 } else {
-    console.log("Всі цифри різні.");
-}
-// Запитуємо у користувача рік народження
-let birthYear = prompt("Введіть свій рік народження:");
-
-// Запитуємо у користувача місто проживання
-let city = prompt("В якому місті ви живете?");
-
-// Запитуємо у користувача улюблений вид спорту
-let sport = prompt("Ваш улюблений вид спорту?");
-
-// Вираховуємо вік користувача
-let currentYear = new Date().getFullYear();
-let age = currentYear - birthYear;
-
-// Визначаємо повідомлення про місто
-let cityMessage;
-switch (city.toLowerCase()) {
-    case "київ":
-        cityMessage = "Ти живеш у столиці України.";
-        break;
-    case "вашингтон":
-        cityMessage = "Ти живеш у столиці США.";
-        break;
-    case "лондон":
-        cityMessage = "Ти живеш у столиці Великобританії.";
-        break;
-    default:
-        cityMessage = `Ти живеш у місті ${city}.`;
-}
-
-// Визначаємо повідомлення про спорт
-let sportMessage;
-switch (sport.toLowerCase()) {
-    case "футбол":
-        sportMessage = "Круто! Хочеш стати як Ліонель Мессі?";
-        break;
-    case "бокс":
-        sportMessage = "Круто! Хочеш стати як Майк Тайсон?";
-        break;
-    case "баскетбол":
-        sportMessage = "Круто! Хочеш стати як Майкл Джордан?";
-        break;
-    default:
-        sportMessage = `Ваш улюблений вид спорту - ${sport}.`;
-}
-
-// Виводимо усі повідомлення у консоль
-console.log(`Ваш вік: ${age}`);
-console.log(cityMessage);
-console.log(sportMessage);
-let numOrStr = prompt('input number or string');
-console.log(numOrStr);
-
-// Видаляємо зайві пробіли на початку та кінці введеного значення
-switch (true) {
-    case numOrStr === null:
-        console.log('ви скасували');
-        break;
-    case numOrStr.trim() === '':
-        console.log('Empty String');
-        break;
-    case isNaN(+numOrStr):
-        console.log('number is Ba_NaN');
-        break;
-    default:
-        console.log('OK!');
-        break;
+    console.log(`${number} не є простим числом.`);
 }
