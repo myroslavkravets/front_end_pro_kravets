72e723a215904b6feeaf7ed69386a5301c93a82d
myroslavkravets <miroslavchikthebest@gmail.com>
2024-09-30 22:47:01 +0300

    new homework

--- Main.js
+++ Main.js
@@ -1,19 +1,24 @@
-let ladder = {
-    step: 0,
-
-    up: function () {
-        this.step++;
-        return this;
-    },
-
-    down: function () {
-        this.step--;
-        return this;
-    },
-
-    showStep: function () {
-        console.log(this.step);
-        return this;
+let company = {
+    sales: [{ name: 'John', salary: 1000 }, { name: 'Alice', salary: 600 }],
+    development: {
+        web: [{ name: 'Peter', salary: 2000 }, { name: 'Alex', salary: 1800 }],
+        internals: [{ name: 'Jack', salary: 1300 }]
     }
 };
-ladder.up().up().down().showStep();
+
+function getTotalSalary(department) {
+    if (Array.isArray(department)) {
+        return department.reduce((total, employee) => total + employee.salary, 0);
+    } else {
+        let sum = 0;
+        for (let subdep of Object.values(department)) {
+            sum += getTotalSalary(subdep);
+        }
+        return sum;
+    }
+}
+
+let totalSalary = getTotalSalary(company);
+console.log(totalSalary); // Виведе загальну суму зарплат
+
+
