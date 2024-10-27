0ff9025183e539ce7fc8637a788515f0a489bb6f
myroslavkravets <miroslavchikthebest@gmail.com>
2024-10-27 14:57:15 +0200

    lesson-11.2

--- Main.js
+++ Main.js
@@ -1,33 +1,13 @@
-// Задаємо розмір таблиці
-const size = 10;
+let isColorChanged = false; // Змінна для відстеження стану кольору
 
-// Створюємо функцію для генерації таблиці Піфагора
-function generateMultiplicationTable(size) {
-    // Починаємо створювати HTML для таблиці
-    let tableHTML = "<table border='1' cellpadding='5'>";
+function toggleTextColor() {
+    const textBlock = document.getElementById("text-block");
 
-    // Перший рядок заголовків стовпців
-    tableHTML += "<tr><th></th>";
-    for (let i = 1; i <= size; i++) {
-        tableHTML += `<th>${i}</th>`;
-    }
-    tableHTML += "</tr>";
-
-    // Додаємо рядки таблиці
-    for (let i = 1; i <= size; i++) {
-        tableHTML += `<tr><th>${i}</th>`; // Заголовок рядка
-        for (let j = 1; j <= size; j++) {
-            tableHTML += `<td>${i * j}</td>`; // Значення добутку
-        }
-        tableHTML += "</tr>";
+    if (isColorChanged) {
+        textBlock.style.color = "black"; // Повертаємо попередній колір
+    } else {
+        textBlock.style.color = "blue"; // Змінюємо на новий колір
     }
 
-    tableHTML += "</table>";
-    return tableHTML;
+    isColorChanged = !isColorChanged; // Змінюємо стан
 }
-
-
-document.body.innerHTML = generateMultiplicationTable(size);
-
-
-
