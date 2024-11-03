bdb7a1c026a550f5c08bfc450a9e1f955de53ac7
myroslavkravets <miroslavchikthebest@gmail.com>
2024-11-03 16:37:26 +0200

    lesson-17.1

--- Main.js
+++ Main.js
@@ -1,85 +1,33 @@
-class Student {
-    constructor(firstName, lastName, birthYear, grades) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthYear = birthYear;
-        this.grades = grades;
-        this.attendance = new Array(25).fill(null); // Масив для відвідуваності з 25 елементами, всі значення спочатку null
+class Calculator {
+    // Метод додавання
+    add(a, b) {
+        return a + b;
     }
 
-    getAge() {
-        const currentYear = new Date().getFullYear();
-        return currentYear - this.birthYear;
+    // Метод віднімання
+    subtract(a, b) {
+        return a - b;
     }
 
-    getAverageGrade() {
-        const total = this.grades.reduce((sum, grade) => sum + grade, 0);
-        return total / this.grades.length;
+    // Метод множення
+    multiply(a, b) {
+        return a * b;
     }
 
-    present() {
-        const index = this.attendance.indexOf(null); // Знаходимо перше порожнє місце в масиві
-        if (index !== -1) {
-            this.attendance[index] = true;
+    // Метод ділення
+    divide(a, b) {
+        if (b === 0) {
+            return "Ділення на нуль неможливе";
         }
-    }
-
-    absent() {
-        const index = this.attendance.indexOf(null); // Знаходимо перше порожнє місце в масиві
-        if (index !== -1) {
-            this.attendance[index] = false;
-        }
-    }
-
-    summary() {
-        const averageGrade = this.getAverageGrade();
-        const attendedClasses = this.attendance.filter(value => value === true).length;
-        const totalClasses = this.attendance.filter(value => value !== null).length;
-        const attendanceRate = attendedClasses / totalClasses;
-
-        if (averageGrade > 90 && attendanceRate > 0.9) {
-            return "Молодець!";
-        } else if (averageGrade > 90 || attendanceRate > 0.9) {
-            return "Добре, але можна краще";
-        } else {
-            return "Редиска!";
-        }
+        return a / b;
     }
 }
 
-const student1 = new Student("Олександр", "Іваненко", 2000, [95, 85, 92, 88, 90]);
-const student2 = new Student("Марія", "Петренко", 1999, [80, 85, 78, 82, 79]);
-const student3 = new Student("Олег", "Сидоренко", 2001, [98, 99, 97, 96, 100]);
+// Приклад використання
+const calc = new Calculator();
 
-student1.present();
-student1.present();
-student1.absent();
-student1.present();
-
-student2.absent();
-student2.absent();
-student2.present();
-student2.present();
-
-student3.present();
-student3.present();
-student3.present();
-student3.present();
-
-console.log(`${student1.firstName} ${student1.lastName}`);
-console.log("Вік:", student1.getAge());
-console.log("Середній бал:", student1.getAverageGrade());
-console.log("Відвідуваність:", student1.attendance);
-console.log("Результат:", student1.summary());
-
-console.log(`${student2.firstName} ${student2.lastName}`);
-console.log("Вік:", student2.getAge());
-console.log("Середній бал:", student2.getAverageGrade());
-console.log("Відвідуваність:", student2.attendance);
-console.log("Результат:", student2.summary());
-
-console.log(`${student3.firstName} ${student3.lastName}`);
-console.log("Вік:", student3.getAge());
-console.log("Середній бал:", student3.getAverageGrade());
-console.log("Відвідуваність:", student3.attendance);
-console.log("Результат:", student3.summary());
+console.log(calc.add(5, 3)); // 8
+console.log(calc.subtract(10, 4)); // 6
+console.log(calc.multiply(3, 6)); // 18
+console.log(calc.divide(8, 2)); // 4
+console.log(calc.divide(8, 0)); // Ділення на нуль неможливе
